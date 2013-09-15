a9256e5efe6915171553a69863423f9db1ed5708
Matth_eu <matthew@matth.eu>
2013-09-15 21:30:52 +0100

    Related Posts

---
+++ hm-related-posts-admin.php
@@ -0,0 +1,171 @@
+<?php
+
+function hm_rp_add_meta_boxes() {
+
+ 	add_meta_box(
+		'hm_rp_mb',
+		'Related Posts',
+		'hm_rp_override_metabox',
+		'post'
+	);
+
+}
+add_action( 'add_meta_boxes', 'hm_rp_add_meta_boxes' );
+
+
+function hm_rp_scripts() {
+
+	wp_enqueue_script( 'select2', HMRP_URL . '/js/select/select2.js', array( 'jquery' ) );
+	wp_enqueue_style( 'select2', HMRP_URL . '/js/select/select2.css' );
+	wp_enqueue_script( 'hm-rp-scripts', HMRP_URL . '/js/hm-rp.js' );
+	wp_enqueue_style( 'hm-rp-styles', HMRP_URL . '/style.css' );
+	wp_enqueue_script( 'field-select', HMRP_URL . '/js/field.select.js', array( 'jquery' ) );
+
+}
+add_action( 'admin_enqueue_scripts', 'hm_rp_scripts' );
+
+function hm_rp_override_metabox( $post ) {
+
+	$data = (array) get_post_meta( $post->ID, 'hm_rp_post' );
+	wp_nonce_field( 'hm_rp','hm_rp_nonce' );
+
+	?>
+	<table class="form-table hm_rp_metabox">
+		<tr>
+			<td style="width: 100%" colspan="12">
+				<div class="hm-rp-field repeatable HMRP_Post_Select" data-rep-min="2" data-rep-max="5">
+
+					<div class="field-title">
+						<label for="hm_rp_post-hm-rp-field-0">Manually select related post</label>
+					</div>
+
+					<div class="hm_rp_metabox_description">By default, 5 related posts are dynamically generated. Manually override these here</div>
+
+					<?php
+
+					foreach ( $data as $key => $value ) :
+						if ( ! empty( $value ) ) :
+							echo '<div class="hm-rp-field-item" data-class="HMRP_Post_Select" style="position: relative">';
+							hm_rp_override_metabox_field( $key, intval( $value ) );
+							echo '</div>';
+						endif;
+					endforeach;
+
+					?>
+
+					<div class="hm-rp-field-item hidden" data-class="HMRP_Post_Select" style="position: relative">
+						<?php hm_rp_override_metabox_field( 'x' ); ?>
+					</div>
+
+					<button class="button hm-rp-repeat-field">Add New</button>
+
+				</div>
+			</td>
+		</tr>
+	</table>
+	<?php
+}
+
+function hm_rp_override_metabox_field( $id, $value = null ) {
+
+	?>
+
+	<a class="hm-rp-delete-field">&times;</a>
+
+	<input id="hm_rp_post-hm-rp-field-<?php echo esc_attr( $id ); ?>" value="<?php echo esc_attr( $value ); ?>"  name="hm_rp_post[hm-rp-field-<?php echo esc_attr( $id ); ?>]"  class="hm_rp_select" data-field-id="hm_rp_post_<?php echo esc_attr( $id ); ?>" style="width: 100%" />
+
+	<?php hm_rp_override_metabox_script( $id, $value ); ?>
+
+	<?php
+}
+
+function hm_rp_override_metabox_script( $id, $value = false, $ajax_args = false ) {
+
+	?>
+
+	<script type="text/javascript">
+
+		jQuery( document ).ready( function() {
+
+			var options = {
+				placeholder: "Type to search" ,
+				allowClear: true
+			};
+
+			var query = JSON.parse( '<?php echo json_encode( $ajax_args ? wp_parse_args( $ajax_args ) : (object) array() ); ?>' );
+
+			options.ajax = {
+				url: '<?php echo add_query_arg( 'action', 'hm_rp_ajax_post_select', admin_url( 'admin-ajax.php' ) ); ?>',
+				dataType: 'json',
+				data: function( term, page ) {
+					query.s = term;
+					query.paged = page;
+					return query;
+				},
+				results : function( data, page ) {
+					return { results: data }
+				}
+			}
+
+			<?php if ( ! empty( $value ) ) : ?>
+
+				options.initSelection = function( element, callback ) {
+
+					var data = <?php echo sprintf( '{ id: %d, text: "%s" }', $value, get_the_title( $value ) ); ?>;
+					callback( data );
+
+				};
+
+			<?php endif; ?>
+
+			if ( 'undefined' === typeof( window.hm_rp_select_fields ) )
+				window.hm_rp_select_fields = {};
+
+			window.hm_rp_select_fields.hm_rp_post_<?php echo esc_attr( $id ); ?> = options;
+
+		} );
+
+	</script>
+
+	<?php
+
+}
+
+function hm_rp_save( $post_id ) {
+
+	if (
+		empty( $_POST['hm_rp_post'] ) ||
+		! isset( $_POST['hm_rp_nonce'] ) ||
+		! wp_verify_nonce($_POST['hm_rp_nonce'], 'hm_rp' )
+	)
+		return;
+
+ 	unset( $_POST['hm_rp_post']['hm-rp-field-x'] );
+
+ 	delete_post_meta( $post_id, 'hm_rp_post' );
+
+ 	foreach ( $_POST['hm_rp_post'] as $value )
+ 		if ( ! empty( $value ) )
+ 			add_post_meta( $post_id, 'hm_rp_post', intval( $value ) );
+
+}
+add_action( 'save_post', 'hm_rp_save' );
+
+// TODO this should be in inside the class
+function hm_rp_ajax_post_select() {
+
+	$query = new WP_Query( $_GET );
+
+	$posts = $query->posts;
+
+	$json = array();
+
+	foreach ( $posts as $post )
+		$json[] = array( 'id' => $post->ID, 'text' => get_the_title( $post->ID ) );
+
+	echo json_encode( $json );
+
+	exit;
+
+}
+add_action( 'wp_ajax_hm_rp_ajax_post_select', 'hm_rp_ajax_post_select' );
---
+++ hm-related-posts.php
@@ -0,0 +1,107 @@
+<?php
+/*
+Plugin Name: HM Related Posts
+Author: Human Made Limited
+Version: 0.1
+Author URI: http://www.hmn.md/
+*/
+
+define( 'HMRP_PATH', str_replace( '\\', '/', dirname( __FILE__ ) ) );
+define( 'HMRP_URL', str_replace( str_replace( '\\', '/', WP_CONTENT_DIR ), str_replace( '\\', '/', WP_CONTENT_URL ), HMRP_PATH ) );
+
+require_once( HMRP_PATH . '/hm-related-posts-admin.php' );
+
+/**
+ * Generates an array of related posts
+ *
+ * @param int $limit. (default: 10)
+ * @param array $post_types. (default: 'post')
+ * @param array $taxonomies. (default: 'post_tag') This is not the taxonomies which are used to compare, it is the taxonomies for the post
+ * @param array $terms_not_in. (default: empty array)
+ * @return array - post IDs
+ */
+function hm_rp_get_related_posts( $limit = 10, $post_types = array( 'post' ), $taxonomies = array( 'post_tag', 'category' ), $terms_not_in = array(), $args = array() ) {
+
+	global $wpdb;
+
+	$default_args = array(
+		'post_id' 	=> get_the_id(),
+		'terms'		=> array(),
+		'related_post_taxonomies' => $taxonomies
+	);
+
+	$args = wp_parse_args( $args, $default_args );
+
+	extract( $args );
+
+	foreach ( $related_post_taxonomies as &$related_post_taxonomy )
+		$related_post_taxonomy = $wpdb->prepare( "'%s'", $related_post_taxonomy );
+
+	if ( empty( $post_id ) )
+		return;
+
+	// Hash of args for cache id
+	$hash = hash( 'crc32', serialize( func_get_args() ) );
+
+	if ( ! $related_posts = wp_cache_get( $post_id . $hash, 'hm_related_posts' ) ) :
+
+		$manual_related_posts = array_filter( get_post_meta( $post_id, 'hm_rp_post' ) );
+		$limit = $limit - count( $manual_related_posts );
+
+		if ( empty( $terms ) )
+			$term_objects = wp_get_object_terms( $post_id, $taxonomies );
+		else
+			$term_objects = $terms;
+
+		$terms = array();
+
+		foreach ( $term_objects as $term )
+			$terms[] = $term->term_id;
+
+		$terms = array_unique( array_diff( $terms, $terms_not_in ) );
+
+		sort( $terms );
+
+		$post_type_sql = '';
+
+		foreach ( $post_types as $post_type )
+			$post_type_sql .= $wpdb->prepare( " OR p.post_type = '%s'", $post_type );
+
+		$post_type_sql = substr( $post_type_sql, 4 );
+
+		$query[] = "SELECT p.ID";
+		$query[] = "FROM $wpdb->term_taxonomy t_t, $wpdb->term_relationships t_r, $wpdb->posts p";
+		$query[] = "WHERE t_t.term_taxonomy_id = t_r.term_taxonomy_id";
+		$query[] = "AND t_r.object_id  = p.ID";
+
+		if ( ! empty( $terms ) ) {
+			// Note $related_post_taxonomies has been 'prepared' already.
+			$query[] = $wpdb->prepare( "AND t_t.term_id IN ( %s )", implode( ', ', $terms ) );
+			$query[] = "AND t_t.taxonomy IN ( " . implode( ', ', $related_post_taxonomies ) . " )";
+		}
+
+		if ( ! empty( $terms_not_in ) ) {
+			$query[] = $wpdb->prepare(
+				"AND p.ID NOT IN ( SELECT tr.object_id FROM wp_term_relationships AS tr INNER JOIN wp_term_taxonomy AS tt ON tr.term_taxonomy_id = tt.term_taxonomy_id WHERE tt.term_id IN ( %s ) )",
+				implode( ', ', $terms_not_in )
+			);
+		}
+
+		$query[] = "AND ( $post_type_sql )";
+		$query[] = $wpdb->prepare( "AND p.ID != %d", $post_id );
+		$query[] = "AND p.post_status = 'publish'";
+		$query[] = "GROUP BY t_r.object_id";
+		$query[] = $wpdb->prepare( "ORDER BY count(t_r.object_id) DESC, p.post_date_gmt DESC LIMIT 0, %d", $limit );
+
+		$query = implode( ' ', $query );
+
+		$related_posts = array_merge( $manual_related_posts, $wpdb->get_col( $query ) );
+		$related_posts = array_map( 'intval', $related_posts );
+
+		wp_cache_add( $post_id . $hash, $related_posts, 'hm_related_posts' );
+
+	endif;
+
+	return $related_posts;
+
+}
